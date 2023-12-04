7850dd72eb8d9ed81f8785cd66d20b3e69745008
Carlos A. R. S <126350847+krlosars@users.noreply.github.com>
2023-12-03 23:15:02 -0300

    Inclusão do esquema de cores Tokyo Night
    Este esquema de cores é implementado através de um plugin disponível no repositório da comunidade do AstroNvim.

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,6 +3,7 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  { import = "astrocommunity.colorscheme.tokyonight-nvim", enabled = true },
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
